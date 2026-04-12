263fa05837936aef047e9e1a9bf72b8d83f4e10c
Guilherme_Alves <Alves@gmail.com>
2026-04-11 23:55:00 -0300

    fix: correcao da url de conexao com o backend

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,54 +1,39 @@
 import partyFetch from "../axios/config";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "../styles/home.css";
+import "../styles/home.css"
 
 function Home() {
+  // Salva as festas
   const [parties, setParties] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
+  // Carrega as festas
   useEffect(() => {
     const loadParties = async () => {
-      try {
-        const res = await partyFetch.get("/party"); // agora correto
-        setParties(res.data);
-      } catch (err) {
-        console.error(err);
-        setError("Erro ao carregar as festas");
-      } finally {
-        setLoading(false);
-      }
+      const res = await partyFetch.get("/party");
+      console.log(res);
+      setParties(res.data);
     };
 
-    loadParties();
+    loadParties()
   }, []);
 
-  if (loading) return <p>Carregando festas...</p>;
-  if (error) return <p>{error}</p>;
+  if (!parties) return <p>Carregando festas...</p>;
 
   return (
     <div className="home">
       <h1>Suas Festas</h1>
-
       <div className="parties-container">
-        {parties.length === 0 ? (
-          <p>Não há festas criadas!</p>
-        ) : (
-          parties.map((party) => (
-            <div className="party" key={party._id}>
-              <img src={party.image} alt={party.title} />
-              <h3>{party.title}</h3>
-
-              <Link
-                to={`/party/${party._id}`}
-                className="btn-secondary"
-              >
-                Detalhes
-              </Link>
-            </div>
-          ))
-        )}
+        {parties.length === 0 && <p>Não há festas criadas!</p>}
+        {parties.map((party) => (
+          <div className="party" key={party._id}>
+            <img src={party.image} alt={party.title} />
+            <h3>{party.title}</h3>
+            <Link to={`/party/${party._id}`} className="btn-secondary">
+              Detalhes
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
