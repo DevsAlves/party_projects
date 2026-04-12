a2385155298619d78479b58e9333718e7f48a0df
Guilherme_Alves <Alves@gmail.com>
2026-04-12 12:18:27 -0300

    fix:correcao na url no back end

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: "https://party-projects-backend-k4kmyz1t3-dev-alves-projects.vercel.app/" +"/api",
+  baseURL: "https://party-projects-backend-k4kmyz1t3-dev-alves-projects.vercel.app/" +"api",
   headers: {
     "Content-Type": "application/json",
   },
