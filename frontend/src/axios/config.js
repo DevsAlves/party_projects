3d4b4c22c50b527ad97fdf3f061f524dc876c742
Guilherme_Alves <Alves@gmail.com>
2026-04-12 11:40:27 -0300

    fix: correcao da url do backend

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: "https://party-projects.onrender.com" + "/api",
   headers: {
     "Content-Type": "application/json",
   },
